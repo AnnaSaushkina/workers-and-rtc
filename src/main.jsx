0bb4aafb6c20a9397250f0de2e38130056da91f9
AnnaSaushkina <saushkinanna@gmail.com>
2026-05-27 17:26:51 +0400

    фикс sw.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js");
+  navigator.serviceWorker.register("./sw.js");
 }
 
 createRoot(document.getElementById("root")).render(
